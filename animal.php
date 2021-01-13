6acf3c6f73e54445507f3650b1c585228481afc3
pindilind <linda.gustafsson@outlook.com>
2021-01-13 20:39:31 +0100

    förberett för arv-klasser

--- animal.php
+++ animal.php
@@ -1,6 +1,7 @@
 <?php 
 
-     class Animal {
+    /* SUPERKLASS */
+    abstract class Animal {
 
         function __construct($coconut, $monkey, $giraffe, $tiger) {
             $this->coconut = $coconut;
@@ -9,10 +10,39 @@
             $this->tiger = $tiger;
         }
 
-        public $coconut;
-        public $monkey;
-        public $giraffe;
-        public $tiger;
+        protected $coconut;
+        protected $monkey;
+        protected $giraffe;
+        protected $tiger;
     
     };
+
+    class coconut extends Animal {
+        function __construct($coconut, $monkey, $giraffe, $tiger) {
+            parent::__construct($coconut, $monkey, $giraffe, $tiger);
+
+        }
+    }
+
+   /*  class monkey extends Animal {
+        function __construct($coconut, $monkey, $giraffe, $tiger, $name) {
+
+            $this->name = $name
+        }
+    }
+
+    class giraffe extends Animal {
+        function __construct($coconut, $monkey, $giraffe, $tiger, $name) {
+
+            $this->name = $name
+        }
+    }
+
+    class tiger extends Animal {
+        function __construct($coconut, $monkey, $giraffe, $tiger, $name) {
+            
+            $this->name = $name
+        }
+    } */
+
 ?>
